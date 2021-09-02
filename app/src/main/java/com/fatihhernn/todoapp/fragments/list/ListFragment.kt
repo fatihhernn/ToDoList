9c159c7070ac61df2d19e649bcc542643d7731af
fatihhernn <fatihhernn@gmail.com>
2021-09-02 11:52:41 +0300

    desing add fragment

--- app/src/main/java/com/fatihhernn/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/fatihhernn/todoapp/fragments/list/ListFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.fatihhernn.todoapp.R
+import com.fatihhernn.todoapp.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
 
+    private var _binding: FragmentListBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding= FragmentListBinding.inflate(inflater,container,false)
+
+        binding.floatingActionButton.setOnClickListener{
+            findNavController() .navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 
 

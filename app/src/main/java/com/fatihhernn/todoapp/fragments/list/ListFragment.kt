662058adb5ca8984e26d0c39607781e286f4ea83
fatihhernn <fatihhernn@gmail.com>
2021-09-17 10:10:32 +0300

    BUG FİX in list view

--- app/src/main/java/com/fatihhernn/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/fatihhernn/todoapp/fragments/list/ListFragment.kt
@@ -3,15 +3,14 @@ package com.fatihhernn.todoapp.fragments.list
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.GridLayout
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 import com.fatihhernn.todoapp.R
 import com.fatihhernn.todoapp.data.models.ToDoData
 import com.fatihhernn.todoapp.data.viewModel.SharedViewModel
@@ -67,7 +66,9 @@ class ListFragment : Fragment(),SearchView.OnQueryTextListener {
 
     private fun setUpRecyclerView() {
         binding.recyclerView.adapter=adapter
-        binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
+        //binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
+        //binding.recyclerView.layoutManager=GridLayoutManager(requireContext(),2)
+        binding.recyclerView.layoutManager=StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
 
         binding.recyclerView.itemAnimator=SlideInUpAnimator().apply {
             addDuration=300
